18a2f17bd28e99f218e168911d647f28d3bace1b
Adam Frank White <adamfrankwhite@gmail.com>
2019-04-22 23:59:42 +0100

    added php modules

---
+++ header.php
@@ -0,0 +1,72 @@
+</head>
+	<body>
+		<header>
+			<img id="logo" class="mx-auto d-block" src="images/logo.jpg" alt="cat logo">
+			<div class="text-center">
+				<a href="index.html" class="text-center"><h1 class="pt-1 mb-0" >CATOPOLIS</h1></a>
+				<p>...a place for cat-lovers</p>
+			</div>
+			<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse" id="navbarNav">
+					<ul id="navigation" class="navbar-nav mx-auto">
+						<li class="nav-item">
+							<a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
+						</li>
+						<li class="nav-item dropdown">
+							<a class="nav-link" href="#projects">Articles</a>
+							<ul class="dropdown-content">
+								<a><li>Environment</li></a>
+								<a><li>Philosophy of Cats</li></a>
+								<a><li>Adopting a cat</li></a>
+								<a><li>Cat care</li></a>
+								<a><li>Cat quotes</li></a>
+								<a><li>Famous cat people</li></a>
+								<a><li>Cat facts</li></a>
+							</ul>
+						</li>
+						<li class="nav-item dropdown">
+							<a class="nav-link" href="blog/blog.html">Cat Care</a>
+							<ul class="dropdown-content">
+								<a><li>Veterinary Care</li></a>
+								<a><li>Fleas and Worms</li></a>
+								<a><li>Common Illnesses</li></a>
+								<a><li>Food</li></a>
+								<a><li>Adopting a cat</li></a>
+							</ul>
+						</li>
+						<li class="nav-item dropdown">
+							<a class="nav-link" href="#projects">Hijinks</a>
+							<ul class="dropdown-content">
+								<a><li>Clever Cats</li></a>
+								<a><li>Silly Cats</li></a>
+								<a><li>Loved up cats</li></a>
+								<a><li>Jelly beans</li></a>
+								<a><li>Happy feet</li></a>
+							</ul>
+						</li>
+						<li class="nav-item dropdown">
+							<a class="nav-link" href="products.html">Shop</a>
+							<ul class="dropdown-content">
+								<a><li>Cat Food/Litter</li></a>
+								<a><li>Cat Equipment/Toys</li></a>
+								<a><li>Medication</li></a>
+								<a><li>Grooming</li></a>
+								<a><li>Clothes/Bags</li></a>
+								<a><li>Cat nicknacks</li></a>
+								<a><li>Books</li></a>
+							</ul>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#contact">Rescue Stories</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#contact">About</a>
+						</li>
+
+					</ul>
+				</div>
+			</nav>
+		</header>
