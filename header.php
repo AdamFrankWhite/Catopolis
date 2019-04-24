8f0141ec725fa39903f98072e582b9f4a2feb68f
Adam Frank White <adamfrankwhite@gmail.com>
2019-04-24 14:29:08 +0100

    split code in modules

--- header.php
+++ header.php
@@ -1,34 +1,35 @@
 </head>
 	<body>
 		<header>
-			<img id="logo" class="mx-auto d-block" src="images/logo.jpg" alt="cat logo">
+			<img id="logo" class="mx-auto d-block" src="/images/logo.jpg" alt="cat logo">
 			<div class="text-center">
-				<a href="index.html" class="text-center"><h1 class="pt-1 mb-0" >CATOPOLIS</h1></a>
+				<a href="index.php" class="text-center"><h1 class="pt-1 mb-0" >CATOPOLIS</h1></a>
 				<p>...a place for cat-lovers</p>
 			</div>
 			<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+
 				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
 					<span class="navbar-toggler-icon"></span>
 				</button>
 				<div class="collapse navbar-collapse" id="navbarNav">
 					<ul id="navigation" class="navbar-nav mx-auto">
 						<li class="nav-item">
-							<a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
+							<a class="nav-link" href="/index.php">Home <span class="sr-only">(current)</span></a>
 						</li>
 						<li class="nav-item dropdown">
-							<a class="nav-link" href="#projects">Articles</a>
+							<a class="nav-link" href="/articles.php">Articles</a>
 							<ul class="dropdown-content">
 								<a><li>Environment</li></a>
 								<a><li>Philosophy of Cats</li></a>
 								<a><li>Adopting a cat</li></a>
 								<a><li>Cat care</li></a>
-								<a><li>Cat quotes</li></a>
-								<a><li>Famous cat people</li></a>
-								<a><li>Cat facts</li></a>
+								<a href="/articles/catquotes.php"><li>Cat quotes</li></a>
+								<a href="/articles/famouspeople.php"><li>Famous cat people</li></a>
+								<a href="/articles/catfacts.php"><li>Cat facts</li></a>
 							</ul>
 						</li>
 						<li class="nav-item dropdown">
-							<a class="nav-link" href="blog/blog.html">Cat Care</a>
+							<a class="nav-link" href="/catcare.php">Cat Care</a>
 							<ul class="dropdown-content">
 								<a><li>Veterinary Care</li></a>
 								<a><li>Fleas and Worms</li></a>
@@ -38,7 +39,7 @@
 							</ul>
 						</li>
 						<li class="nav-item dropdown">
-							<a class="nav-link" href="#projects">Hijinks</a>
+							<a class="nav-link" href="/hijinks.php">Hijinks</a>
 							<ul class="dropdown-content">
 								<a><li>Clever Cats</li></a>
 								<a><li>Silly Cats</li></a>
@@ -48,7 +49,7 @@
 							</ul>
 						</li>
 						<li class="nav-item dropdown">
-							<a class="nav-link" href="products.html">Shop</a>
+							<a class="nav-link" href="/shop.php">Shop</a>
 							<ul class="dropdown-content">
 								<a><li>Cat Food/Litter</li></a>
 								<a><li>Cat Equipment/Toys</li></a>
@@ -59,11 +60,16 @@
 								<a><li>Books</li></a>
 							</ul>
 						</li>
-						<li class="nav-item">
-							<a class="nav-link" href="#contact">Rescue Stories</a>
+						<li class="nav-item dropdown">
+							<a class="nav-link" href="/community.php">Community</a>
+							<ul class="dropdown-content">
+								<a><li>Your cat pics</li></a>
+								<a><li>Rescue stories</li></a>
+								<a><li>In Memory</li></a>
+							</ul>
 						</li>
 						<li class="nav-item">
-							<a class="nav-link" href="#contact">About</a>
+							<a class="nav-link" href="/about.php">About</a>
 						</li>
 
 					</ul>
